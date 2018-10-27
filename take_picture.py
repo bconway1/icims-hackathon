23b8c40b420ba2dff48d693bb2137fcd93cd51a8
Sean Jones <sjones7@stevens.edu>
2018-10-27 14:51:01 -0400

    adding take picture test file

---
+++ take_picture.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import cv2, sys
+
+# Define constants
+DEVICE_NUMBER = 0
+
+# Initialize webcam
+vc = cv2.VideoCapture(DEVICE_NUMBER)
+
+# Check if the webcam works
+if vc.isOpened():
+    # Try to get the first frame
+    retval, frame = vc.read()
+else:
+    # Exit the program
+    sys.exit(1)
+
+# Read in the next frame
+retval, frame = vc.read()
+
+# Show the frame to the user
+cv2.imshow("DragonBoard 410c Workshop", frame)
+
+# Exit program after waiting indefinitely for a pressed key
+cv2.waitKey(0)
